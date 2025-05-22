296d1ac18433546efe1f2bb8cb36ff7a9e2aee4c
aSa82279iaH <mosque-assays.0u@icloud.com>
2025-05-22 13:48:53 +0200

    Fix a bug causing incorrect data display in the file

--- 9UUFb/qW1/interactive-classroom_HfDT_1742785971.data.py
+++ 9UUFb/qW1/interactive-classroom_HfDT_1742785971.data.py
@@ -22,7 +22,6 @@ def parseCommand(command):
 
 if __name__ == '__main__':
     Classroom.initialize()
-
     student = Classroom.service.userProfiles().get(userId="me").execute()
     name = student.get("name").get("fullName")
 
